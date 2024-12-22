b0edb9c7eb55b5db73ee3046e5d9b9076e90397d
sourav6563 <160314416+sourav6563@users.noreply.github.com>
2024-12-22 10:44:40 +0000

    commit

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,18 @@
 "use strcict";// its means newer version of javascript  its use to solve the problem with old vs new
+// alert
+
+let name = "sourav mahato"
+let age = 24
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value null is object
+// undefined=> its always undefined
+// symbol=> unique
+// object
+console.log(typeof 25);
