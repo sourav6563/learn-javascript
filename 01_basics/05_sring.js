2cffd7d2cc04fb446dacc77b6046649d40f4eb0f
sourav6563 <160314416+sourav6563@users.noreply.github.com>
2024-12-24 19:01:30 +0000

    continue

--- 01_basics/05_sring.js
+++ 01_basics/05_sring.js
@@ -5,7 +5,7 @@ const repocount = 6
 
 console.log(`hello my name is ${name} and my repo count is  ${repocount}`);
 
-const sexyname = new String ("banku")
+const sexyname = new String ("banku-mahato-bm")
 console.log(sexyname[4]);
 console.log(sexyname.__proto__);
 
@@ -13,4 +13,30 @@ console.log(sexyname.length);
 console.log(sexyname.toLocaleUpperCase());
 console.log(sexyname.charAt(0));
 console.log(sexyname.indexOf("b"));
+
+const newstring = sexyname.substring(0,10)
+console.log(newstring);
+
+const anotherstring = sexyname.slice(-15,4) //slice is use for negaive value
+console.log(anotherstring);
+
+const newstringone = "    sourav   "
+console.log(newstringone);
+console.log(newstringone.trim());
+
+
+
+const url = "https://www.youtube.com"
+console.log(url.replace('com','in'));
+console.log(url.includes('sunny'));
+console.log(sexyname.split('-'));
+
+
+
+
+
+
+
+
+
 
