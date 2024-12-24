923fd7ed59a632aec69c0318e0c367f8262f6170
sourav6563 <160314416+sourav6563@users.noreply.github.com>
2024-12-24 07:44:23 +0000

    c

---
+++ 01_basics/05_sring.js
@@ -0,0 +1,16 @@
+//sring consider in both '' ""  quotes in js
+const name = "sourav"
+const repocount = 6
+// console.log(name + repocount);
+
+console.log(`hello my name is ${name} and my repo count is  ${repocount}`);
+
+const sexyname = new String ("banku")
+console.log(sexyname[4]);
+console.log(sexyname.__proto__);
+
+console.log(sexyname.length);
+console.log(sexyname.toLocaleUpperCase());
+console.log(sexyname.charAt(0));
+console.log(sexyname.indexOf("b"));
+
