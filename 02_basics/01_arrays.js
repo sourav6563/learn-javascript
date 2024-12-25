324998a15b5553cec26602035495536798a390e3
sourav6563 <160314416+sourav6563@users.noreply.github.com>
2024-12-25 18:32:59 +0000

    continue

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,35 @@
+// arrays
+
+const myarr = [0, 1, 2, 3, 4, 5]; //array must be in [] bracket
+const myheroes = ["ironman", "spiderman", "shakiman"];
+const myarray2 = new Array(1, 5, 8, 9);
+// console.log(myarr[4]);
+// array method
+
+// myarr.push(10)
+// myarr.push(15)
+// myarr.pop()
+// myarr.unshift(18) // problem with unshift is always shows in first or array order some time its cause isue so majorly devs avoid that
+// myarr.shift() //its like push its remove unshift thing or first what it will be its use for first order remove and push use for last order remove
+
+// console.log( myarr.includes(18));  //its used for that thing is available or not true or false in boolean
+// console.log( myarr.indexOf(8)); // index of use that number or txt where count
+
+// const newarray = myarr.join()
+// console.log(myarr);
+
+// console.log(newarray);
+// console.log(typeof newarray);
+
+// slice,splice imp for interview
+console.log("A", myarr);
+const mynewarray = myarr.slice(1, 3);
+console.log("B", myarr);
+
+console.log(mynewarray);
+
+const mynewarray2 = myarr.splice(1, 3);
+console.log("C", myarr);
+
+console.log(mynewarray2);
+//main thing is to understand in slice if select array its shows but main array not manipulated but in case of c example  where splice use array select evrything that we select but main problem is main array is manipulated
